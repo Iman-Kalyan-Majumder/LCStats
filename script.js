858763f333c4f1b1fc6a982161b9885348839938
Iman Kalyan Majumder <77956318+Iman-Kalyan-Majumder@users.noreply.github.com>
2022-06-11 15:12:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 async function get()
 {
-  document.getElementById('data').style.display='none';
-  document.getElementById('checker').style.visibility='visible';
-  document.getElementById('checker').innerHTML='Loading...';
-  let user=document.getElementById('enter').value;
-  let link="https://leetcode-stats-api.herokuapp.com/"+user;
-  let data=await fetch(link);
-  let res=await data.json();
-  let msg=res.status.toString();
-  console.log(msg);
-  if(msg==="success")
-    {  
-      document.getElementById('checker').style.visibility='hidden';
-      document.getElementById('totalSolved').innerHTML=res.totalSolved.toString();
-      document.getElementById('easy').innerHTML=res.easySolved.toString();
-      document.getElementById('medium').innerHTML=res.mediumSolved.toString();
-      document.getElementById('hard').innerHTML=res.hardSolved.toString();
-      document.getElementById('data').style.display='flex';
-      document.getElementById('data').style.alignItems='center';
-      document.getElementById('data').style.justifyContent='space-evenly';
-      document.getElementById('data').style.flexWrap='wrap';
-      document.getElementById('square').style.display='flex';
-      document.getElementById('square').style.alignItems='center';
-      document.getElementById('square').style.justifyContent='center';
-    }
-  else
+    let user=document.getElementById('enter').value;
+    if(user==="")
     {
-      document.getElementById('checker').innerHTML="User not found";
-      document.getElementById('data').style.display='none';
+        document.getElementById('checker').style.visibility='visible';
+        document.getElementById('checker').innerHTML='Please enter a username...';
+    }
+    else
+    {
+        document.getElementById('data').style.display='none';
+        document.getElementById('checker').style.visibility='visible';
+        document.getElementById('checker').innerHTML='Loading...';
+        let link="https://leetcode-stats-api.herokuapp.com/"+user;
+        let data=await fetch(link);
+        let res=await data.json();
+        let msg=res.status.toString();
+        console.log(msg);
+        if(msg==="success")
+        {  
+            document.getElementById('checker').style.visibility='hidden';
+            document.getElementById('totalSolved').innerHTML=res.totalSolved.toString();
+            document.getElementById('easy').innerHTML=res.easySolved.toString();
+            document.getElementById('medium').innerHTML=res.mediumSolved.toString();
+            document.getElementById('hard').innerHTML=res.hardSolved.toString();
+            document.getElementById('data').style.display='flex';
+            document.getElementById('data').style.alignItems='center';
+            document.getElementById('data').style.justifyContent='space-evenly';
+            document.getElementById('data').style.flexWrap='wrap';
+            document.getElementById('square').style.display='flex';
+            document.getElementById('square').style.alignItems='center';
+            document.getElementById('square').style.justifyContent='center';
+        }
+        else
+        {
+            document.getElementById('checker').innerHTML="User not found";
+            document.getElementById('data').style.display='none';
+        }
     }
 }
